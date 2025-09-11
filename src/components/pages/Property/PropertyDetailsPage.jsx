9aadd2ca88442e87c77eadde7988ddf70d24d10d
Gireeshma27 <gireeshma053@gmail.com>
2025-09-11 14:30:41 +0530

    updates on property detail page

--- src/components/pages/Property/PropertyDetailsPage.jsx
+++ src/components/pages/Property/PropertyDetailsPage.jsx
@@ -822,7 +822,7 @@ const PropertyDetailsPage = () => {
       className="wishlist-icon"
       onClick={() => toggleWishlist(property.id)}
     >
-      {isInWishlist(property.id) ? (
+      {isInWishlist? (
         <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" width="24" height="24">
           <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 
                    4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81 
